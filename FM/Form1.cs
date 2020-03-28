05ca09b97fda15e75f473f3203dd6c0772684855
Rudolf <mirey_woodson@mail.ru>
2020-03-28 07:52:43 +0300

    tray

--- FM/Form1.cs
+++ FM/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace FM
 {
---
+++ FTP/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace FTP
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            // Создаем объект FtpWebRequest
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://192.168.94.1/test.txt");
+            // устанавливаем метод на загрузку файлов
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.EnableSsl = true;
+
+            // если требуется логин и пароль, устанавливаем их
+            //request.Credentials = new NetworkCredential("login", "password");
+            //request.EnableSsl = true; // если используется ssl
+
+            // получаем ответ от сервера в виде объекта FtpWebResponse
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            // получаем поток ответа
+            Stream responseStream = response.GetResponseStream();
+            // сохраняем файл в дисковой системе
+            // создаем поток для сохранения файла
+            FileStream fs = new FileStream("newTest.txt", FileMode.Create);
+
+            //Буфер для считываемых данных
+            byte[] buffer = new byte[64];
+            int size = 0;
+
+            while ((size = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                fs.Write(buffer, 0, size);
+
+            }
+            fs.Close();
+            response.Close();
+
+            Console.WriteLine("Загрузка и сохранение файла завершены");
+            Console.Read();
+        }
+    }
+}
+        
+    
+
