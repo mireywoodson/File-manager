5bdd10d0a9d5327af8d7447aa208add511b7cd05
Rudolf <111rudolf@users.noreply.github.com>
2020-03-28 04:34:40 +0300

    Добавьте файлы проекта.

---
+++ FM/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        //Кнопка "Перейти"
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+
+            DirectoryInfo di = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] d1 = di.GetDirectories();
+
+            foreach (DirectoryInfo dd in d1)
+            {
+
+                listBox1.Items.Add(dd);
+
+            }
+
+            FileInfo[] files = di.GetFiles();
+
+            foreach (FileInfo dd in files)
+            {
+
+                listBox1.Items.Add(dd);
+
+            }
+        }
+
+        //Двойной щелчок в listbox'e
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = Path.Combine(textBox1.Text,listBox1.SelectedItem.ToString());
+
+            listBox1.Items.Clear();
+
+            DirectoryInfo di = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] d1 = di.GetDirectories();
+
+            foreach (DirectoryInfo dd in d1)
+            {
+
+                listBox1.Items.Add(dd);
+
+            }
+
+            FileInfo[] files = di.GetFiles();
+
+            foreach (FileInfo dd in files)
+            {
+
+                listBox1.Items.Add(dd);
+
+            }
+        }
+
+        //Кнопка "Назад"
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text[textBox1.Text.Length - 1] == '\\')
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+            else if (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+            {
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+
+            listBox1.Items.Clear();
+
+            DirectoryInfo di = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] d1 = di.GetDirectories();
+
+            foreach (DirectoryInfo dd in d1)
+            {
+
+                listBox1.Items.Add(dd);
+
+            }
+
+            FileInfo[] files = di.GetFiles();
+
+            foreach (FileInfo dd in files)
+            {
+
+                listBox1.Items.Add(dd);
+
+            }
+        }
+    }
+}
---
+++ Файловый менеджер/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Файловый_менеджер
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            foreach (DirectoryInfo cd in dirs)
+            {
+                listBox1.Items.Add(cd);
+            }
+
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo cf in files)
+            {
+                listBox1.Items.Add(cf);
+            }
+            
+        }
+    }
+}
